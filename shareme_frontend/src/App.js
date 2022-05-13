091ee52e48f6e911f5231f9b5e301800ceb63975
Aman Bisht <amanvista@gmail.com>
2022-05-13 15:33:50 +0530

    2nd Commit

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
+import GoogleLogin from 'react-google-login';
 
 const App = () => {
+  const navigate = useNavigate();
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+};
+
 
 export default App
