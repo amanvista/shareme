928578b037b3de2f73696d574e2975bce5650f20
Aman Bisht <amanvista@gmail.com>
2022-05-16 21:34:42 +0530

    Pin working

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import Spinner from './Spinner';
 import { useParams } from 'react-router-dom';
+import { feedQuery, searchQuery } from '../utils/data';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
 
 
 const Feed = () => {
     const { categoryId } = useParams();
 const ideaName = categoryId || 'new';
 const [loading, setLoading] = useState(true);
+const [pins, setPins] = useState();
+
+
+useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
+
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+  
 if (loading) {
     return (
       <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
@@ -14,7 +38,9 @@ if (loading) {
   }
   return (
     <div>
-        Feeds
+        {pins && (
+        <MasonryLayout pins={pins} />
+      )}
     </div>
   )
 }
